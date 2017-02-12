dff932f62c45a476da9e03f1de68ea63aea190d6
goumang2010 <goumang2010@live.com>
2017-02-12 18:30:05 +0800

    init

---
+++ index.js
@@ -0,0 +1,48 @@
+var $source = document.getElementById('src-code');
+var $target = document.getElementById('target-code');
+var $convert = document.getElementById('convert');
+var $clean = document.getElementById('clean');
+var $copy = document.getElementById('copy');
+$clean.addEventListener('click', function () {
+    $source.value = '';
+});
+$copy.addEventListener('click', function () {
+    $target.select();
+    document.execCommand("copy");
+});
+$convert.addEventListener('click', function () {
+    var srctext = $source.value;
+    $target.value = convert(srctext);
+});
+
+function convert(srctxt) {
+    if (!srctxt) {
+        return '';
+    }
+    var prei = 0;
+    var pres = [];
+    var _src = srctxt;
+    return '---\ntitle: \nexcerpt: \ncategories: \n- FE\n---\n\n' +
+        _src
+            .replace(/\s*<pre>\s*([\s\S]+?)\s*<\/pre>\s*/mg, function (m, p1) {
+                pres.push(p1);
+                return '${' + (prei++) + '}';
+            })
+            .replace(/^(\*+)\s*(.+?)\s*\n/mg, '$1 $2\n')
+            .replace(/^(#+)\s*(.+?)\s*\n/mg, function (m, p1, p2) {
+                return (p1.replace(/#/g, '-') + ' ' + p2 + '\n');
+            })
+            // .replace(/\n*<\/?pre>\n*/g, '\n```\n')
+            .replace(/\n*\s*(=+)\s*(.+?)(\s*=+\s*)\n+/mg, function (m, p1, p2, p3) {
+                return ('\n\n' + p1.replace(/=/g, '#') + ' ' + p2 + '\n');
+            })
+            .replace(/(?:\[{1,2}\s*)?((f|ht){1}(tp|tps):\/\/([\w-]+\.)+[\w-]+(\/[\w- ./?%&=]*)?)(?:\s*\]{1,2})?(?!\()/g, function (m, p1) {
+                return '[' + p1 + '](' + p1 + ')';
+            })
+            .replace(/\[\[\s*(.+)\s*\]\]/g, function (m, p1) {
+                return '[' + p1 + '](' + p1 + ')';
+            })
+            .replace(/\${(\d+)}/g, function (m, p1) {
+                return '\n```\n' + pres[p1] + '\n```\n';
+            });
+}
